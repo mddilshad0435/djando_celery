00c552041988bcb985c912aa3c85b9f30351e8d0
mddilshad0435 <mddilshad0435@gmail.com>
2022-03-22 10:49:48 +0530

    secret key

--- celery_django/celery_django/settings.py
+++ celery_django/celery_django/settings.py
@@ -145,5 +145,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'dilshad.ongraph@gmail.com'
-EMAIL_HOST_PASSWORD = 'qyusnqjaycgiqxnu'
+EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = 'Celery <dilshad.ongraph@gmail.com>'
